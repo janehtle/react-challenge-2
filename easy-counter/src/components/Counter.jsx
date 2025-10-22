2a5969c70df12f39d5df5c51ecd118990f4c7b61
Jane Le <janehtle@gmail.com>
2025-10-22 09:52:36 -0400

    created counter and hosted

---
+++ easy-counter/src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Counter() {
+    const [count, setCount] = useState(0);   //holds the current count
+    const increment = () => setCount(c => c += 1); //c = count
+    const decrement = () => setCount(c => c -= 1);
+    const reset = () => setCount(0); //reset counter back to 0
+
+    return (
+        <>
+            <h1>Counter: {count}</h1>
+            <div>
+                <button onClick={decrement}>-</button>
+                <button onClick={increment}>+</button>
+                <button onClick={reset}>Reset</button>
+            </div>
+        </>
+    )
+}
+
+export default Counter
