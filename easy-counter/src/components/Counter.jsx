295e81a8abf4657a25fcbdfb37a62d4435b7a9b7
Jane Le <janehtle@gmail.com>
2025-10-22 10:14:04 -0400

    readme added and comments

--- easy-counter/src/components/Counter.jsx
+++ easy-counter/src/components/Counter.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 function Counter() {
     const [count, setCount] = useState(0);   //holds the current count
+    //count is a piece of state and setCount is a setter/function, created by useState
+
     const increment = () => setCount(c => c += 1); //c = count
     const decrement = () => setCount(c => c -= 1);
     const reset = () => setCount(0); //reset counter back to 0
